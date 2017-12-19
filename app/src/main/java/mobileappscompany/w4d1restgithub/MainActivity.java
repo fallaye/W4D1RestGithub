e002310eda20a4b97833cc528b54fc116c7b8ee3
fallaye <leger.leger20@gmail.com>
2017-12-19 13:33:03 -0500

    Initial commit

---
+++ app/src/main/java/mobileappscompany/w4d1restgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package mobileappscompany.w4d1restgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
